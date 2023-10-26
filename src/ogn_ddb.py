5356bb99bc56d504159118b943b20e2f1cb2a8fc
Moritz Althaus <moritzalthaus@gmx.de>
2023-10-26 17:26:52 +0200

    format with black

--- src/ogn_ddb.py
+++ src/ogn_ddb.py
@@ -6,8 +6,11 @@ ogn_ddb_url = "https://ddb.glidernet.org/download/?j=1&t=1"
 
 def download_ddb():
     ddb = requests.get(ogn_ddb_url).json()
-    aircraft = {v: {"id": 1, "name": v} for v in set([v["aircraft_model"] for v in ddb["devices"]])}
-    
+    aircraft = {
+        v: {"id": 1, "name": v}
+        for v in set([v["aircraft_model"] for v in ddb["devices"]])
+    }
+
     with open("ogn_ddb.json", "w") as outfile:
         json.dump(ddb, outfile, indent=4)
 
@@ -17,4 +20,3 @@ def download_ddb():
 
 if __name__ == "__main__":
     download_ddb()
-    
