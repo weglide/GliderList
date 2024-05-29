60a554e7545bc1dfef0bdade8ea7636e952a6a78
Moritz Althaus <moritzalthaus@gmx.de>
2024-05-29 08:38:58 +0200

    feat: add newly added gliders to ogn mapping

--- tests/test_all_mapped.py
+++ tests/test_all_mapped.py
@@ -5,6 +5,7 @@ OGN_DDB_URL = "https://ddb.glidernet.org/download/?j=1&t=1"
 
 # aircrafts which we do not want to track
 no_mapping = [
+    "Swift 3-E",
     "Breguet 904",
     "MikroKopter Quadro",
     "DJI S800",
